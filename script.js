7443c1eb36583c6ecf0a1613482d46bcfc2d2371
2850-johan <penthiumchristian@gmail.com>
2025-04-04 09:18:43 +0200

    avant dernier ajout de la phase finale

--- script.js
+++ script.js
@@ -7,3 +7,37 @@ menuOpenButton.addEventListener("click", () => {
 
 //Close the menu when clicking on the close button
 menuCloseButton.addEventListener("click", () => menuOpenButton.click());
+
+//Initialize Swiper
+const swiper = new Swiper('.slider-wrapper', {
+    loop: true,
+    // permet de meyttre le curseur
+    grabCursor : true,
+    spacebetween: 25,
+  
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true, // pemet de rendre dynamique les images
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    // Responsive breakpoints
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        },
+    },
+  });
